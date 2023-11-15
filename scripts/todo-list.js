9883595c63b6e359e0ba223fd9cb73189578969e
joseperla1 <131569908+joseperla1@users.noreply.github.com>
2023-11-14 19:06:08 -0600

    Add files via upload

---
+++ scripts/todo-list.js
@@ -0,0 +1,49 @@
+const todoList =[];
+
+renderTodoList();
+function renderTodoList(){
+    let todoListHTML = '';
+
+	todoList.forEach((taskObject, index) =>{
+				const { name, dueDate } = taskObject;
+        const html = `
+				<div>${name}</div>
+				<div>${dueDate}</div>
+				<button  class="delete-todo-button js-delete-todo-button"> Delete </button>
+				`;
+        todoListHTML +=html;
+
+	});
+
+    document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+
+	document.querySelectorAll('.js-delete-todo-button')
+	.forEach((deleteButton, index) =>{
+		deleteButton.addEventListener('click', ()=>{
+			todoList.splice(index,1);
+			renderTodoList();
+		});
+	});
+}
+
+document.querySelector('.js-add-todo-button')
+.addEventListener('click',()=>{
+	addTodo();
+});
+
+function addTodo(){
+    const inputElement = document.querySelector('.js-name-input');
+		const dateInputElement = document.querySelector('.js-due-date-input');
+		const dueDate = dateInputElement.value;
+    const name = inputElement.value;
+    todoList.push({
+			name,
+			dueDate
+		});
+
+    inputElement.value ='';
+		dateInputElement.value='';
+
+    renderTodoList();
+}
